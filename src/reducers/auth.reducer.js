828e3800ed8745d6b2959431bed2bff49d3eaa2b
mohammad ashhar <ashhark31@gmail.com>
2023-12-25 00:41:02 +0530

    changes_in_reducers_auth

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -3,7 +3,7 @@ import {createSlice} from '@reduxjs/toolkit'
 export const authSlice = createSlice({
     name: 'authentication',
     initialState: {
-        isTokenAvailable: false
+        isTokenAvailable: null
     },
     reducers: {
         setTokenAuth: (state,action) => {
